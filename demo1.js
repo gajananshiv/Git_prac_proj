85892239c56fd0fa7ec82660d94229b91ebc63e3
unknown <pragatirajas24@gmail.com>
2026-08-16 14:39:57 +0530

    add line6

--- demo1.js
+++ demo1.js
@@ -1,5 +1,5 @@
 console.log("line number 1")
 console.log("line number 2")
 console.log("line number3")
-
+console.log("add line number 5")
 
--- demo3.js
+++ demo3.js
@@ -7,3 +7,4 @@ console.log("add new file line2");
 
 console.log("add new file line4");
 console.log("add new file line");
+console.log("add line no 6");
